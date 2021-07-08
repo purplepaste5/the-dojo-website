4a67475431612d37a9830b123cc1a60dee537e92
Andrew Skowronski <andrewskowronski@Andrews-Mac-mini.local>
2021-07-08 09:30:36 -0400

    wipeout

--- .eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  plugins: ['react'],
-  parser: 'babel-eslint',
-};
--- src/Components/SlantedTextContainer/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SlantendTextContainer = ({ children }) => {
-  return <div className="slanted">{children}</div>;
-};
-
-export default SlantendTextContainer;
